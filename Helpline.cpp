d3827cfc14854fcf3c832e7f4314c20f4b05a8b3
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-06-17 17:55:03 +0530

    password changer made

--- Helpline.cpp
+++ Helpline.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class center_string{
-  public:
-  void centerstring(string s, int width)
-  {
-    int l = s.length();
-    int pos = (int)((width - l) / 2);
-    for (int i = 0; i < pos; i++)
-      cout << " ";
-    cout << s;
-  }
-};
+void centerstring(string s, int width)
+{
+  int l = s.length();
+  int pos = (int)((width - l) / 2);
+  for (int i = 0; i < pos; i++)
+    cout << " ";
+  cout << s;
+}
+
 // class caller_info
 // {
 //   char name[50];
@@ -79,32 +77,40 @@ class Password
 public:
   Password() { password = "0000"; }
   string get_password() { return password; }
+  void set_password()
+  {
+    cout << endl;
+    centerstring("Enter the new password: ", 80);
+    cin >> password;
+
+    // s.admin_page();
+  }
+
 };
 // Show
 
-class Show : public Password ,public center_string
+class Show : public Password
 {
 public:
-center_string cs;
   int password_screen()
-    {
-    Password p;
+  {
+
     string p_in;
     int chances = 3;
-    cs.centerstring("Welcome", 80);
+    centerstring("Welcome", 80);
     cout << endl;
     while (chances)
     {
       cout << endl;
-      cs.centerstring("Enter Your Admin Password:", 80);
+      centerstring("Enter Your Admin Password:", 80);
       cin >> p_in;
-      if (p_in == p.get_password())
+      if (p_in == Password::get_password())
       {
         return 1;
       }
       else
       {
-        cs.centerstring(" Wrong Password", 80);
+        centerstring(" Wrong Password", 80);
         cout << endl
              << setw(32) << --chances << " chances remaining";
       }
@@ -114,38 +120,94 @@ center_string cs;
   int welcome_screen()
   {
     system("cls");
-    cs.centerstring("Welcome Administrator", 80);
-    cout<<endl<<endl;
-    cs.centerstring("Press Enter to continue",80);
+    centerstring("Welcome Administrator", 80);
+    cout << endl
+         << endl;
+    centerstring("Press Enter to continue", 80);
     cin.ignore();
     cin.get();
 
-    cout<<"\nHave a nice day";
     return 1;
   }
-};
+  int admin_page()
+  {
+  label1:
+    system("cls");
+    centerstring("Admin Menu", 80);
+    cout << endl
+         << endl;
+    centerstring("1. Enter New Caller Info", 80);
+    cout << endl;
+    centerstring("2. View Emergency Unit Status", 80);
+    cout << endl;
+    centerstring("3. View Previous Callers", 80);
+    cout << endl;
+    centerstring("4. Change Admin Password", 80);
+    cout << endl;
+    centerstring("5. LogOut", 80);
+    cout << endl
+         << endl;
+    centerstring("Enter Your choice...", 80);
+    unsigned int choice;
 
-
+    cin >> choice;
+    if (choice > 5 || choice <= 0)
+    {
+      centerstring("Invalid Input...press enter to try again...", 80);
+      cin.ignore();
+      cin.get();
+      goto label1;
+    }
+    else
+      return choice;
+  }
+} s;
 
 int main()
 {
-  Show s;
-  int temp = s.password_screen();
-  if (temp)
-  {
-    s.welcome_screen();
+
+  if (s.password_screen())
+  { // cout<<s.welcome_screen();
+    if (s.welcome_screen())
+    {
+      switch (s.admin_page())
+      {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        s.set_password();
+        cout << s.get_password();
+        // s.admin_page();
+        break;
+      case 5:
+        // s.password_screen();
+        break;
+      default:
+        cout << "This will never be invoked";
+      }
+    }
   }
   else
   {
     system("cls");
-    center_string cs;
-    cs.centerstring("You Failed to Enter the correct Password", 80);
+
+    centerstring("You Failed to Enter the correct Password", 80);
   }
 
-  // cout << "\033[2J\033[1;1H";
+  class Admin_screen_actions : public Password
+  {
+  public:
+    // get_caller_info();
+  };
 
-  int x;
-  cin >> x;
+  cout << "\n\nPress Enter to exit program...";
+
+  cin.ignore();
+  cin.get();
   return 0;
   // caller_info c1;
   // c1.get_caller_name();
